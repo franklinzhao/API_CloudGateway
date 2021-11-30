f9d85aa9e3981ad7e8f9ca4c67be495fcadcca38
franklinzhao <Guolin.zhao@outlook.com>
2021-11-29 22:06:08 -0500

    updated.

--- cloud-gateway/src/main/java/com/zhao/cloud/gateway/FallBackMethodController.java
+++ cloud-gateway/src/main/java/com/zhao/cloud/gateway/FallBackMethodController.java
@@ -1,4 +1,4 @@
-package com.dailycodebuffer.cloud.gateway;
+package com.zhao.cloud.gateway;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
