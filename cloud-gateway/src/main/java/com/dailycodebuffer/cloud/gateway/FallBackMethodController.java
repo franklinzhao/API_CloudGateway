5c8585282f5fb8d2ef2dd497431697e8d580dfff
franklinzhao <zhaogu@sheridancollege.ca>
2021-11-29 21:49:04 -0500

    API cloud gateway
    API cloud gateway

---
+++ cloud-gateway/src/main/java/com/dailycodebuffer/cloud/gateway/FallBackMethodController.java
@@ -0,0 +1,23 @@
+package com.dailycodebuffer.cloud.gateway;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
+
+@RestController
+public class FallBackMethodController {
+	@HystrixCommand(fallbackMethod="userServiceFallBackMethod")
+    @GetMapping("/userServiceFallBack")
+    public String userServiceFallBackMethod() {
+        return "User Service is taking longer than Expected." +
+                " Please try again later";
+    }
+	@HystrixCommand(fallbackMethod="departmentServiceFallBackMethod")
+    @GetMapping("/departmentServiceFallBack")
+    public String departmentServiceFallBackMethod() {
+        return "Department Service is taking longer than Expected." +
+                " Please try again later";
+    }
+}
